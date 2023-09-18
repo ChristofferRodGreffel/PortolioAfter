3ed1fb715e4f6b72ee6db4b2e96d969b2ffc5b81
ChristofferRodGreffel <119002008+ChristofferRodGreffel@users.noreply.github.com>
2023-09-18 15:28:13 +0200

    Responsive styling continued

--- src/components/ReturnButton.jsx
+++ src/components/ReturnButton.jsx
@@ -4,7 +4,6 @@ export default function ReturnButton() {
     const project = document.querySelector("#c-right");
     project.style.transform = "translateX(150%)";
     menu.style.transform = "translateX(0%)";
-    console.log("You clicked the button!");
   };
 
   return <i className="fa-regular fa-hand-point-left" onClick={menuReturn}></i>;
